c8638b43338d9004a05838b3fd248411ba8df15e
hyooliim422 <76100840+hyooliim422@users.noreply.github.com>
2020-12-16 23:43:41 +0900

    Add files via upload

---
+++ Unsplash/Unsplash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unsplash
+//
+//  Created by 김효림 on 2020/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unsplash/Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by 김효림 on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
